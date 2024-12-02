1737d05a7a015d482c4520e256105831450527e0
Michell Wambui Mbogo <mishie254@gmail.com>
2024-12-02 18:50:10 +0600

    arrow lab

---
+++ index.js
@@ -0,0 +1,10 @@
+// divide function
+const divide = function() {
+    return 2000 / 100;
+};
+
+// square function
+const square = (num) => num * num;
+
+// add function
+const add = (a, b) => a + b;
